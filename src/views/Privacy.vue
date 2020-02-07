12e99166512668c44adf0f8b9fa88a91f3af2680
Adrix2238 <2238adrix@gmail.com>
2020-02-07 13:03:50 +0100

    pricavy in progress

--- src/views/Privacy.vue
+++ src/views/Privacy.vue
@@ -6,47 +6,10 @@
       </h1>
       <div class="textspace">
         <div class="text font-regular">
-          <p>
-            La presente Política de Privacidad establece los términos en que
-            umbra usa y protege la información que es proporcionada por sus
-            usuarios al momento de utilizar su sitio web. Este proyecto está
-            comprometido con la seguridad de los datos de sus usuarios. Cuando
-            le pedimos llenar los campos de información personal con la cual
-            usted pueda ser identificado, lo hacemos asegurando que solo se
-            empleará de acuerdo con los siguientes términos.
-          </p>
-          <p>
-            Nuestro sitio web podrá recoger información personal, por ejemplo:
-            Nombre, situación geográfica y año de nacimiento. Umbra emplea la
-            información con el fin de proporcionar el mejor servicio posible,
-            particularmente para crear una mayor experiencia inmersiva y mejorar
-            nuestro servicio. Estamos altamente comprometido para cumplir con el
-            compromiso de mantener su información segura.
-          </p>
-          <p>
-            Una cookie se refiere a un fichero que es enviado con la finalidad
-            de solicitar permiso para almacenarse en su ordenador, al aceptar
-            dicho fichero se crea y las cookies sirven entonces para tener
-            información respecto al tráfico web, y también facilita las futuras
-            visitas a una web recurrente. Nuestro sitio web no emplea las
-            cookies para poder identificar las páginas que son visitadas y su
-            frecuencia.
-          </p>
-          <p>
-            Este sitio web contiene enlaces a otros sitios que pudieran ser de
-            su interés. Una vez que usted de clic en estos enlaces y abandone
-            nuestra página, ya no tenemos control sobre al sitio al que es
-            redirigido y por lo tanto no somos responsables de los términos o
-            privacidad ni de la protección de sus datos en esos otros sitios
-            terceros. Dichos sitios están sujetos a sus propias políticas de
-            privacidad por lo cual es recomendable que los consulte para
-            confirmar que usted está de acuerdo con estas.
-          </p>
-          <p>
-            Este proyecto no venderá, cederá ni distribuirá la información
-            personal que es recopilada sin su consentimiento, salvo que sea
-            requerido por un juez con un orden judicial.
-          </p> 
+
+          <p v-html="($t('privacy.description'))"></p>
+
+         
         </div>
       </div>
     </div>
